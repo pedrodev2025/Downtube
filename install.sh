7e7de18766af1bdb371f857055660911f3879d2a
pedrodev2025 <pedrolucasinvestidor.github@gmail.com>
2025-03-10 19:27:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ sudo apt install wget
 sudo apt update 
 sudo apt install nodejs npm
 wget https://github.com/pedrodev2025/Downtube/raw/refs/heads/main/archives.zip 
-unzip archives.zip /Downtube 
-cd /Downtube/archives 
-sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp 
-sudo chmod a+rx /usr/local/bin/yt-dlp
+unzip archives.zip
+cd /archives
+sudo apt install python3
+sudo apt install python3-pip
 sudo npm install express
 sudo npm start
 echo O Servidor Foi Iniciado Acesse Com a Porta 3000!
