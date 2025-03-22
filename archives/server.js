f87de8ca440657de2550b20d1f41648216f6d511
pedrodev2025 <pedrolucasinvestidor.github@gmail.com>
2025-03-22 12:42:42 -0300

    Update server.js

--- archives/server.js
+++ archives/server.js
@@ -1,22 +1,25 @@
-  const express = require('express');
+const express = require('express');
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+// Caminho absoluto para a pasta "public"
+const publicPath = path.join(__dirname, 'public');
+
 // Servir arquivos estáticos da pasta "public"
-app.use(express.static(path.join(__dirname, 'Downtube-1.1', 'public')));
+app.use(express.static(publicPath));
 
 // Rota raiz para servir o index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Downtube-1.1', 'public', 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 // Rota para baixar para o servidor
 app.post('/download-to-server', (req, res) => {
     const videoUrl = req.body.url;
-    const outputPath = path.join(__dirname, 'Downtube-1.1', 'downloads', '%(title)s.%(ext)s');
+    const outputPath = path.join(__dirname, '..', 'downloads', '%(title)s.%(ext)s');
 
     exec(`yt-dlp -o "${outputPath}" ${videoUrl}`, (error, stdout, stderr) => {
         if (error) {
@@ -31,7 +34,7 @@ app.post('/download-to-server', (req, res) => {
 // Rota para baixar para o dispositivo
 app.post('/download-to-device', (req, res) => {
     const videoUrl = req.body.url;
-    const tempFilePath = path.join(__dirname, 'Downtube-1.1', 'temp_video.mp4');
+    const tempFilePath = path.join(__dirname, '..', 'temp_video.mp4');
 
     exec(`yt-dlp -o ${tempFilePath} -f mp4 ${videoUrl}`, (error, stdout, stderr) => {
         if (error) {
