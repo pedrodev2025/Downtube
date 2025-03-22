bbd5c245794dcb4e1fa2d05fef1479950c3631c5
pedrodev2025 <pedrolucasinvestidor.github@gmail.com>
2025-03-22 12:20:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ sudo apt install wget
 sudo apt update 
 sudo apt install nodejs npm
 wget https://github.com/pedrodev2025/Downtube/archive/refs/tags/1.1.zip 
-unzip archives.zip
-cd /archives
+unzip 1.1.zip
+cd /Downtube-1.1
 sudo apt install python3
 sudo apt install python3-pip
 sudo npm install express
